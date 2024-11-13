2398c814e93230dbfe94d7d89d095762e7cc30da
Majo Alvarez <majo.bootcamp@gmail.com>
2024-11-12 22:22:07 -0600

    changes to package

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,11 +6,7 @@ import type { Request, Response } from 'express';
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 import { authenticateToken } from './services/auth.js';
-// import { fileURLToPath } from 'url';
 
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
@@ -43,12 +39,6 @@ const startApolloServer = async () => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
 
-    // const distDir = path.resolve('client', 'dist');
-    // app.use(express.static(distDir));
-  
-    // app.get('*', (_req: Request, res: Response) => {
-    //   res.sendFile(path.join(distDir, 'index.html'));
-    // });
   }
 
   app.listen(PORT, () => {
