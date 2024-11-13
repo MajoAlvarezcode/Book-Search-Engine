088e0d1554e6ac81c34e0e3ddfe15b79b629bba9
Majo Alvarez <majo.bootcamp@gmail.com>
2024-11-12 22:27:06 -0600

    Server changed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import type { Request, Response } from 'express';
 import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 import { authenticateToken } from './services/auth.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const server = new ApolloServer({
